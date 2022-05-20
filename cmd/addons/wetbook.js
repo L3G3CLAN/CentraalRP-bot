56c9c4571bf1695266e2d9aff3ee8d638aa696c5
L3G3CLAN <81651393+L3G3CLAN@users.noreply.github.com>
2022-05-20 13:31:41 +0200

    Update wetbook.js

--- cmd/addons/wetbook.js
+++ cmd/addons/wetbook.js
@@ -3,13 +3,13 @@ const discord = require("discord.js");
 module.exports.run = async (client, message, args) => {
 
     var botEmbed = new discord.MessageEmbed()
-        .setTitle("Wetbook Optus Roleplay")
+        .setTitle("Wetbook Centraal Roleplay")
         .setDescription("Je krijgt de wetbook alls je op de knop onder dit bericht klickt. \n **!! LET OP !! Het wetbook kan veranderen.**")
         .setColor(process.env.COLLOR)
         .setThumbnail(process.env.LOGO)
 //        .setImage(process.env.BANNER)
         .setTimestamp()
-        .setFooter("Wetbook Optus Roleplay")
+        .setFooter("Wetbook Centraal Roleplay")
         
         const row = new discord.MessageActionRow().addComponents(
 
@@ -17,7 +17,7 @@ module.exports.run = async (client, message, args) => {
             .setLabel("Wetbook")
             .setStyle("LINK")
             .setEmoji("📝")
-            .setURL("https://optusrp.nl/wetbook")
+            .setURL("https://centraalrp.nl/wetbook")
     
         );
 
